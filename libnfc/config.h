b2915a79cf61b2337acfd49b6b421900832b7197
Alberto Lopez <Alberto2k7@gmail.com>
2024-03-30 17:16:57 -0300

    fix(git): config.h uningnored

---
+++ libnfc/config.h
@@ -0,0 +1,10 @@
+/*
+ * config.h
+ *
+ *  Created on: 2019-04-21 17:46
+ *      Author: Jack Chen <redchenjs@live.com>
+ */
+
+#define WIN32
+#define PACKAGE_VERSION "1.8.0"
+#define DRIVER_PN532_UART_ENABLED
